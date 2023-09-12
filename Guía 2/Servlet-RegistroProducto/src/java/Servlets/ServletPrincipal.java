9a46f3482674620e87ebedd61532733b6703e4f3
vereSlam <126106491+vereSlam@users.noreply.github.com>
2023-09-12 15:50:53 -0600

    Servlets

---
+++ Guía 2/Servlet-RegistroProducto/src/java/Servlets/ServletPrincipal.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author verri
+ */
+@WebServlet(name = "ServletPrincipal", urlPatterns = {"/ServletPrincipal"})
+public class ServletPrincipal extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>REGISTRO PRODUCTO</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletPrincipal at " + request.getContextPath() + "</h1>");
+            
+            String nombre = request.getParameter("nombreProducto");
+            String precio = request.getParameter("precio");
+            String cantidad = request.getParameter("cantidad");
+            String entradaProducto = request.getParameter("fecha");
+            String descripcion = request.getParameter("descripcion");
+            String categoria = request.getParameter("categoria");
+            
+            out.println("<h1>Envío de producto exitoso</h1>");
+            out.println("<strong>Nombre del producto: </strong>"+ nombre);
+            out.println("<br><strong>Precio: </strong>" + precio);
+            out.println("<br><strong>Cantidad: </strong>" + cantidad);
+            out.println("<br><strong>Entrada del producto: </strong>" + entradaProducto);
+            out.println("<br><strong>Categoria: </strong>" + categoria);
+            out.println("<br><strong>Descripcion: </strong>" + descripcion);
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
